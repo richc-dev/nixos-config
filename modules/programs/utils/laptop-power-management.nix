9194190b1605b52e206881892984d8f934836bfc
richc-dev <rcc@richardchurch.com>
2025-09-19 17:02:06 -0500

    Add laptop power management

---
+++ modules/programs/utils/laptop-power-management.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+{
+  config = lib.mkIf config.c-opt.laptop {
+    environment.systemPackages = with pkgs; [
+      powertop
+    ];
+
+    services = {
+      # Enable TLP
+      # https://nixos.wiki/wiki/Laptop
+      # https://wiki.archlinux.org/title/TLP
+      # https://linrunner.de/tlp/settings/index.html
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+          CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+          CPU_MIN_PERF_ON_AC = 0;
+          CPU_MAX_PERF_ON_AC = 100;
+          CPU_MIN_PERF_ON_BAT = 0;
+          CPU_MAX_PERF_ON_BAT = 20;
+
+          # Helps with long term battery health
+          START_CHARGE_THRESH_BAT0 = 40;
+          STOP_CHARGE_THRESH_BAT0 = 80;
+        };
+      };
+
+      # Enable upower
+      upower = {
+        enable = true;
+      };
+
+    };
+
+  };
+}
