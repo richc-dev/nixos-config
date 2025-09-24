767d5b84e60fcd2c3fc3652ca001b43f608d66c8
richc-dev <rcc@richardchurch.com>
2025-09-24 10:37:45 -0500

    Fixes

--- modules/programs/default.nix
+++ modules/programs/default.nix
@@ -5,13 +5,17 @@
     ./audio-video-progs.nix
     ./browsers.nix
     ./dev-tools.nix
+    ./librewolf.nix
     ./packages.nix
     ./vscode.nix
   ];
 
   c-opt = {
     audio-video-progs.enable = true;
-    browsers.enable = true;
+    browsers = {
+      enable = true;
+      librewolf.enable = true;
+    };
     vscodium.enable = true;
   };
 }
--- modules/programs/packages.nix
+++ modules/programs/packages.nix
@@ -25,7 +25,7 @@
 
       thunderbird
 
-      material-design-icons
+      material-symbols
     ];
 
     programs = {
