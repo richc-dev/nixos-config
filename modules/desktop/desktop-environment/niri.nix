ec60f09415e44ca90941fa022b70d4dcd170d6b7
richc-dev <rcc@richardchurch.com>
2025-11-04 07:44:17 -0600

    Add niri

---
+++ modules/desktop/desktop-environment/niri.nix
@@ -0,0 +1,19 @@
+{ config, lib, pkgs, ... }:
+{
+  options.c-opt.de.niri.enable = mkEnableOption "Enable Niri compositor";
+
+  config = lib.mkIf config.c-opt.de.niri.enable {
+    programs.niri.enable = true;
+
+    services.gnome.gnome-keyring.enable = true;
+
+    environment.systemPackages = [
+      xwayland-satellite # xwayland support
+      mako
+    ];
+
+    xdg.portal.extraPortals = [ xdg-desktop-portal-gnome ];
+
+    #home-manager.users.${config.c-opt.user.name}.xdg.configFile."niri/config.kdl".source = ./configs/niri/config.kdl;
+  };
+}
