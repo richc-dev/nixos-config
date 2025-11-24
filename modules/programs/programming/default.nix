15e4616606a817d972bf2ff339b95723d2bdd993
richc-dev <rcc@richardchurch.com>
2025-11-24 15:27:27 -0600

    Add direnv

--- modules/programs/programming/default.nix
+++ modules/programs/programming/default.nix
@@ -9,6 +9,8 @@
   options.c-opt.programs.programming.enable = lib.mkEnableOption "Development tools";
 
   config = lib.mkIf config.c-opt.programs.programming.enable {
+    programs.direnv.enable = true;
+
     home-manager.users.${config.c-opt.user.name}.home = {
       packages = with pkgs; [
         css-html-js-minify
