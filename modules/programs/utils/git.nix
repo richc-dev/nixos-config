fe69173f288871f90d012a5020adfbb9757089c4
richc-dev <rcc@richardchurch.com>
2025-12-24 10:15:52 -0600

    Update git config

--- modules/programs/utils/git.nix
+++ modules/programs/utils/git.nix
@@ -10,6 +10,15 @@
         name = "${config.c-opt.user.fullName}";
         email = "${config.c-opt.user.email}";
       };
+      sendemail = {
+        smtpserver = "richc.dev";
+        smtpuser = "rcc@richc.dev";
+        smtpencryption = "ssl";
+        smtpserverport = 465;
+      };
+      credential = {
+        helper = "cache --timeout 1800";
+      };
     };
   };
 }
