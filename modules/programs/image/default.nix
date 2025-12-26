914f6fd97ed4881e15102b164b8bcc1f19953e6a
richc-dev <rcc@richardchurch.com>
2025-12-26 14:56:36 -0600

    Add OpenTabletDrivers

--- modules/programs/image/default.nix
+++ modules/programs/image/default.nix
@@ -5,14 +5,25 @@
   options.c-opt.programs.image.enable = lib.mkEnableOption "Install image editing/viewing programs";
 
   config = lib.mkIf config.c-opt.programs.image.enable {
+    services.udev = {
+      enable = true;
+      packages = with pkgs; [ libwacom ];
+    };
+
+    hardware.opentabletdriver = {
+      enable = true;
+      daemon.enable = true;
+    };
 
     home-manager.users.${config.c-opt.user.name}.home = {
       packages = with pkgs; [
         aseprite
-	gimp
+	      gimp
         inkscape-with-extensions
         nomacs
         krita
+        scribus
+        libwacom
       ];
     };
 
